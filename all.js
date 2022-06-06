6f73d29009530e3b00afc19255b3b75b3727a9e8
Knn <w19951129326@yahoo.com.tw>
2022-06-07 01:33:15 +0800

    完成第二頁

--- all.js
+++ all.js
@@ -1,3 +1,33 @@
 $(".top").click(function(){
     $("html").animate({scrollTop:0},700);
-})
+});
+$(".top-2").click(function(){
+    $("html").animate({scrollTop:0},700);
+});
+$(".number").click (function(event){
+    event.preventDefault();
+    $(this).toggleClass("bg-blue");
+    $(this).parent().siblings().find(".number").removeClass("bg-blue");
+
+    newNumber = $(this).find(".people").text();
+    $(".plan-people").text(newNumber);
+    if(newNumber==">25000"){
+        $(".cost").text("洽客服")
+        $(".standard-cost").text("洽客服");
+    }else{
+        $(".cost").text(newNumber *0.06);
+        $(".standard-cost").text(newNumber *0.16);
+    }
+});
+
+$(".QA-title").on("click", function (event) {
+    event.preventDefault();
+    $(this).toggleClass("blue")
+    $(this).parent().siblings().find(".QA-title").removeClass("blue");
+
+    $(this).toggleClass("plus").toggleClass("minus");
+    $(this).parent().siblings().find(".QA-title").removeClass("minus").addClass("plus");
+
+    $(this).siblings(".answer").slideToggle();
+    $(this).parent().siblings().find(".answer").slideUp();
+});
